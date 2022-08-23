e8920815296cd934cc6572b3419b2274839b1b58
Namelss4 <jeronimocano22@gmail.com>
2022-08-23 09:45:08 -0500

    Slow funcionando

--- arduinoApp.ino
+++ arduinoApp.ino
@@ -2,6 +2,10 @@
 #include "task2.h"
 #include "task3.h"
 #include "task4.h"
+#include "events.h"
+
+buttonEvt_t buttonEvt = {false, BUTTONS::NONE};
+
 
 void setup()
 {
--- task4.cpp
+++ task4.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "task4.h"
+#include "events.h"
 
 
 void task4(){
